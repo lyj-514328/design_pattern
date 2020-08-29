6b073181194e930982dfbab152f6a7d2528c0329
LYJ514328 <764451199@qq.com>
2020-08-30 01:36:27 +0800

    update

--- 0_template_model/0_1.cpp
+++ 0_template_model/0_1.cpp
@@ -1,46 +1,50 @@
 //需求：设计输入输出文件，传入对象，实现对对象的遍历，同时使用用户的输出函数
 #include <iostream>
 #include <iterator>
+#include <string>
+#include <vector>
 using namespace std;
+template<class T>
+class output_item {
+public:
+	T* item;
+	output_item(void) {
+		item = nullptr;
+	}
+	output_item(T& obj) {
+		item = &obj;
+	}
+	virtual void* output(T::iterator&) {
+		//do nothing;
+	}
+	virtual ~output_item(void) {
+		//do nothing;
+	}
 
-template <class T>
-class output_item
-{
-    T *item;
-    output_item(void)
-    {
-        item = nullptr;
-    }
-    output_item(T &obj)
-    {
-        item = &obj;
-    }
-
-    virtual void *output(T::iterator &)
-    {
-        //do nothing;
-    }
-    void for_each_item(void)
-    {
-        for (auto it = item.begin(); it != item.end(); it++)
-        {
-            output(it);
-        }
-    }
-    template <typename Function>
-    void for_each_item(Function func)
-    {
-        for (auto it = item.begin(); it != item.end(); it++)
-        {
-            output(it);
-            if (func(it))
-                break;
-        }
-    }
-    virtual ~output_item(void){
-        //do nothing;
-    }
+protected:
+	void for_each_item(void) {
+		for (auto it = item.begin(); it != item.end(); it++) {
+			output(it);
+		}
+	}
+	template<typename Function>
+	void for_each_item(Function func) {
+		for (auto it = item.begin(); it != item.end(); it++) {
+			output(it);
+			if (func(it)) break;
+		}
+	}
 };
- int main(void)
-{
+template<class T>
+class Output: public: output_item {
+protected:
+	virtual void* output(T::iterator& it) {
+		cout << *it << endl;
+	}
+};
+int main(void) {
+	vector<const string> test;
+	test.push_back("hell world");
+	//To Do
+	return 0;
 }
